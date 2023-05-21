3764b3bcbce1c9c6b45c01c620880713aab7629d
Yurii Herasin <yurii.herasin@tu-ilmenau.de>
2023-05-21 14:57:51 +0200

    Color schema

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
   content: [
     "./src/**/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
+    colors: {
+      black: '#242728',
+      white: colors.white,
+      gray: colors.gray,
+      primary: '#f2ab55',
+      secondary: {
+        DEFAULT: '#7ea1c4',
+        'light': '#e8eef3',
+        'lighter': '#f5f8fc',
+        'light-middle':'#7E9AC4',
+        'middle': '#3F4F8D',
+        'dark': '#1c355d',
+        'darkblue':'#06152a',
+        'primary-hover':'#EF9C01',
+        'light-middle-hover':'#3B65A5',
+      },
+    },
     extend: {},
   },
   plugins: [],
