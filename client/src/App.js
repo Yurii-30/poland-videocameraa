45c6ebefa043e391bc20ac5e89fc5017f908d519
Yurii Herasin <yurii.herasin@tu-ilmenau.de>
2023-05-23 23:07:46 +0200

    Information transfer from Server to Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-function App() {
-  return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
-    </div>
-  );
-}
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+ 
+// We import all the components we need in our app
+import Main from "./components/main";
+ 
+const App = () => {
+ return (
+   <div>
+     <Routes>
+       <Route exact path="/" element={<Main />} />
+     </Routes>
+   </div>
+ );
+};
+
 export default App;
