456d9c75569c8a21840fa75251637bc2735cef49
Yurii Herasin <yurii.herasin@tu-ilmenau.de>
2023-05-26 19:27:42 +0200

    change path to React templates

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,7 @@
 const colors = require('tailwindcss/colors');
 module.exports = {
   content: [
-    "./src/**/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     colors: {
