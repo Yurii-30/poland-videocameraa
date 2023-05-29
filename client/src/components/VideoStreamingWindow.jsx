7d9c7071a6c3007eec132ffe9f1988fbef6184c6
Yurii Herasin <yurii.herasin@tu-ilmenau.de>
2023-05-29 02:30:40 +0200

    Video Translator

---
+++ client/src/components/VideoStreamingWindow.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect} from "react";
+import ReactPlayer from "react-player";
+
+const VideoStreamingWindow = (props) => {
+    const [ videoURL, setVideoURL ] = useState(props.link);
+    return (
+        <div>
+            <ReactPlayer
+                className = "react-player"
+                url = {videoURL}    
+                width = "100%"
+                height = "75%"
+                controls
+                playing
+            />
+        </div>
+    );
+
+}
+
+export default VideoStreamingWindow;
