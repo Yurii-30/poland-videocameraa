aab1c2f7ea9609367e689b171ebacd0bb2be3eb6
Yurii Herasin <yurii.herasin@tu-ilmenau.de>
2023-05-29 02:29:34 +0200

    add VideoStreamingWindow

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -2,17 +2,19 @@ import { GoogleMap, Marker, InfoWindowF } from "@react-google-maps/api";
 import styles from "./map_styles.css";
 import { useMemo } from "react";
 import { useEffect, useState } from "react";
+import VideoStreamingWindow from "./VideoStreamingWindow";
 
 const Map = () => {
     const [markerCoordinates, setMarkerCoordinates] = useState([]);
     const [activeMarker, setActiveMarker] = useState(null);
     const [mapReference, setMapReference] = useState();
     const [infoWindowF, setInfoWindowF] = useState();
+    const [videoStreamingWindow, setVideoStreamingWindow] = useState();
     const [isOpen, setIsOpen] = useState(false);
-    const geo_center_Poland = process.env.REACT_APP_POLAND_GEO_CENTER.split(',');
-    const geo_center = useMemo(() => ({ lat: parseFloat(geo_center_Poland[0].toString()), lng: parseFloat(geo_center_Poland[1].toString()) }), []);
+    const [isPreview, setIsPreview] = useState(false);
+    const geo_center_Poland = process.env.REACT_APP_POLAND_GEO_CENTER.toString().split(',');
+    const geo_center = useMemo(() => ({ lat: parseFloat(geo_center_Poland[0]), lng: parseFloat(geo_center_Poland[1]) }), []);
     
-    console.log("Geographical center of Poland: lang ", geo_center_Poland[0]," long: ", geo_center_Poland[1]);
     const handleActiveMarker = (marker) => {
         if (marker === activeMarker) {
             return;
@@ -30,9 +32,15 @@ const Map = () => {
 
     const handleMarkerClick = (id, title, location, lat, lng) => {
         mapReference?.panTo({lat, lng});
-        setInfoWindowF({ id, title, location });
         setIsOpen(true);
+        setIsPreview(false);
+        setInfoWindowF({ id, title, location });
     };
+
+    const handleMarkerDoubleClick = (id, livestream_availability, link) => {
+        setIsPreview(true);
+        setVideoStreamingWindow({ id, livestream_availability, link });
+    }
 
     useEffect(() => {
         async function getMarkerCoordinates() {
@@ -57,35 +65,58 @@ const Map = () => {
 
     return (
         <div className = "text-3xl font-bold underline">
+        {
+            isPreview &&
+            <div>
+                <VideoStreamingWindow
+                    link = {videoStreamingWindow.link}
+                />
+            </div>
+        }
+        {
+            !isPreview &&
             <GoogleMap
                 center = {geo_center}
                 zoom = {6.5}
                 onClick = {() => setActiveMarker(null)}
                 mapContainerClassName = "map-container"
             >
-            {           
-                markerCoordinates?.map(({ _id, title, location, latitude, longitude }) => ( 
-                    <Marker
-                        key = {_id} 
-                        position = {{lat: latitude, lng: longitude}}
-                        onClick = {() => {
-                            handleMarkerClick(_id, title, location, latitude, longitude);
-                        }}
-                    >
-                        {isOpen && infoWindowF.id === _id && (
-                            <InfoWindowF
-                                onCloseClick={() => {
-                                    setIsOpen(false);
-                                }}
-                            >
-                                <h2>{infoWindowF.title}, {infoWindowF.location}</h2>
-                            </InfoWindowF>
-                        )}
-                    </Marker>
-                ))
-            }
+                {           
+                    markerCoordinates?.map(({ _id, title, location, latitude, longitude, livestream_availability, link }) => ( 
+                        <Marker
+                            key = {_id} 
+                            position = {{lat: latitude, lng: longitude}}
+                            icon={{
+                                path: window.google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+                                fillOpacity: 1,
+                                fillColor: "white",
+                                strokeColor: "red",
+                                scale: 5,
+                              }}
+                            onClick = {() => {
+                                handleMarkerClick(_id, title, location, latitude, longitude);
+                            }}
+                            onDblClick = {() => {
+                                // transition to videotranslation page
+                                handleMarkerDoubleClick(_id, livestream_availability, link);
+                            }}
+                        >
+                            {
+                                isOpen && infoWindowF.id === _id && (
+                                    <InfoWindowF
+                                        onCloseClick={() => {
+                                            setIsOpen(false);
+                                        }}
+                                    >
+                                        <h2>{infoWindowF.title}, {infoWindowF.location}</h2>
+                                    </InfoWindowF>
+                                )
+                            }
+                        </Marker>
+                    ))
+                }
             </GoogleMap>
-        
+        }
         </div>
     );
 }
